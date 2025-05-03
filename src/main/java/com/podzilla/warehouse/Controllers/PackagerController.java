3738f7e337d490a160dfc6ef86d90050f7779bd3
Yosef <65974533+koka-afk@users.noreply.github.com>
2025-05-03 06:43:20 +0300

    Proposed Setup (#20)
    * Proposed Setup
    
    * Added Maven Plugin
    
    * added actual credentials
    
    * Added H2 Temporarily

---
+++ src/main/java/com/podzilla/warehouse/Controllers/PackagerController.java
@@ -0,0 +1,9 @@
+package com.podzilla.warehouse.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/warehouse/packager")
+public class PackagerController {
+}
