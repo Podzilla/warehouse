1ef9a0f4d3a7e89f2b00bc982cc4d5f17f168010
Ahmed Wael <73046395+ahmedwael216@users.noreply.github.com>
2025-05-17 15:20:09 +0300

    Rabbit mq (#40)
    * add factory design pattern
    
    * added mvn dependancy
    
    * started integration with library
    
    * started integration with library
    
    * update .gitignore
    
    * update .gitignore

--- src/main/java/com/podzilla/warehouse/WarehouseApplication.java
+++ src/main/java/com/podzilla/warehouse/WarehouseApplication.java
@@ -3,13 +3,15 @@ package com.podzilla.warehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
+@ComponentScan(basePackages = {"com.podzilla"})
 public class WarehouseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WarehouseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApplication.class, args);
+    }
 
 }
