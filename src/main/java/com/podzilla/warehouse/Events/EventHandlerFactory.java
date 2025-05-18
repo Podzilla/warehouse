149bd05ba2c551a64cc5026110250d5b89248291
Ahmed Wael <ahmedwael216@protonmail.com>
2025-05-18 23:41:15 +0300

    added empty handler

--- src/main/java/com/podzilla/warehouse/Events/EventHandlerFactory.java
+++ src/main/java/com/podzilla/warehouse/Events/EventHandlerFactory.java
@@ -12,11 +12,14 @@ import java.util.HashMap;
 @Component
 public class EventHandlerFactory {
     private final HashMap<Class<? extends BaseEvent>, EventHandler<? extends BaseEvent>> handlers;
+    private final EmptyEventHandler emptyEventHandler;
 
     @Autowired
     public EventHandlerFactory(OrderPlacedEventHandler OrderPlacedEventHandler,
                                OrderStockReservationEventHandler orderStockReservationEventHandler,
-                               OrderCancelledEventHandler orderCancelledEventHandler) {
+                               OrderCancelledEventHandler orderCancelledEventHandler,
+                               EmptyEventHandler emptyEventHandler) {
+        this.emptyEventHandler = emptyEventHandler;
         handlers = new HashMap<>();
         handlers.put(OrderPlacedEvent.class, OrderPlacedEventHandler);
         handlers.put(OrderStockReservationRequestedEvent.class, orderStockReservationEventHandler);
@@ -27,7 +30,7 @@ public class EventHandlerFactory {
     public <T extends BaseEvent> EventHandler<T> getHandler(T event) {
         EventHandler<? extends BaseEvent> handler = handlers.get(event.getClass());
         if (handler == null) {
-            throw new IllegalArgumentException("No handler found for event type: " + event.getClass().getName());
+            return (EventHandler<T>) emptyEventHandler;
         }
         return (EventHandler<T>) handler;
     }
